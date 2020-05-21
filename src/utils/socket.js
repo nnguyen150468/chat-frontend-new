38ae409eb0c2ba1a23ccf2305f946462d0ce34c1
Nguyen Nguyen <n.dinhnguyen95@gmail.com>
2020-05-21 17:49:42 +0700

    added heroku connection

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,6 @@
 import socketIOClient from 'socket.io-client';
-const socket = socketIOClient("http://localhost:5000")
+// const socket = socketIOClient("http://localhost:5000")
+const socket = socketIOClient("https://nguyen-chat.herokuapp.com/")
+
 
 export default socket
